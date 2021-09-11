14f3a2260fac11c2082e44a0f4e5f5e499443e70
Taner Safet <75890463+taner-97@users.noreply.github.com>
2021-09-11 22:36:16 +0300

    Footer Section And Design MenuItems

---
+++ script.js
@@ -0,0 +1,11 @@
+let MenuItem = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight == "0px"){
+        MenuItems.style.maxHeight = "200px";
+    }else{
+        MenuItems.style.maxHeight == "0px";
+    }
+}
